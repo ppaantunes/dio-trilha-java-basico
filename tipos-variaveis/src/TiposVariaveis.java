17a707b97fd16f8cbe71d2e525940897ad6c49ff
Pedro <pedro.antunes1@outlook.com>
2023-03-03 22:57:53 -0300

    criando projeto para exemplo de tipos operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,33 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+            //tipos primitivos.
+            //estudar a classe String que representa texto na aplicação.
+
+
+        String meuNome = "PEDRO PAULO";
+
+        double salarioMinimo = 1300;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; 
+            // O cast realizado acima (short) é uma adaptação, pega um tipo mais abrangente e transforma em um tipo correspondente,
+            // melhor evitar esse recurso pois pode afetar em uma altereção de um valor com precisão maior para menor,
+            // pode acontecer algo inesperado, melhor evitar essa prática.
+
+        int numero = 1;
+        
+        numero = 2; 
+            // No caso podemos alterar o valor das variáveis, caso nunca possa ser mudado usaremos de uma variável com a indicação de constante = final
+            // e escrita em CAIXA ALTA, porém devemos atentar de colocar a constante final, pois só a caixa alta não representa uma constante.
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+            // Compreendemos agora que na declaração de uma variável como uma constante, ou seja que não poderá sofrer alteração, utilizamos a palavra final,
+            // que por convenção deverá ser escrita toda em caixa alta, como explicito acima no VALOR_DE_PI.
+
+
+
     }
 }
